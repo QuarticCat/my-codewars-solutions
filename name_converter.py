b4767c023ea5903d7e35d50f8110623948a5f2fa
QuarticCat <QuarticCat@protonmail.com>
2020-09-10 00:41:47 +0800

    update

--- name_converter.py
+++ name_converter.py
@@ -1,11 +1,4 @@
+import re
 import sys
-from urllib.parse import quote_plus, unquote_plus
 
-if len(sys.argv) != 3:
-    print('Too many or too few arguments')
-elif sys.argv[1] == 'encode':
-    print(quote_plus(sys.argv[2]))
-elif sys.argv[1] == 'decode':
-    print(unquote_plus(sys.argv[2]))
-else:
-    print('Unknown argument: ', sys.argv[1])
+print(re.sub(r'[^a-zA-Z]+', '-', sys.argv[1]))
